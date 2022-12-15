f2707ac44b1e7b19854d049b8ecaa2bbd61485d2
bzmillerboy <bzmiller82@gmail.com>
2022-12-15 10:18:13 -0500

    send email for webhook

--- functions/emphasys-webhook/emphasys-webhook.js
+++ functions/emphasys-webhook/emphasys-webhook.js
@@ -1,21 +1,47 @@
-const Sentry = require('@sentry/serverless')
-const { SENTRY_CLIENT_KEY, ENV_NAME } = process.env
+const Sentry = require("@sentry/serverless");
+const { SENTRY_CLIENT_KEY, ENV_NAME } = process.env;
+const sgMail = require("@sendgrid/mail");
+const {
+  SENDGRID_API_KEY,
+  SENDGRID_FROM_EMAIL,
+  SENDGRID_FROM_NAME,
+  HUBSPOT_API_KEY,
+  HUBSPOT_PORTAL_ID,
+  URL,
+} = process.env;
 
 Sentry.AWSLambda.init({
   dsn: `https://${SENTRY_CLIENT_KEY}.ingest.sentry.io/5499762`,
   environment: ENV_NAME,
-  tracesSampleRate: 1.0
-})
+  tracesSampleRate: 1.0,
+});
 
-exports.handler = Sentry.AWSLambda.wrapHandler(async (event, context, callback) => {
-  const payload = JSON.parse(event.body)
-  Sentry.setContext('character', {
-    payload: payload,
-    debug: true
-  })
-  console.log(event)
-  return {
-    statusCode: 200,
-    body: `Webhook received`
+exports.handler = Sentry.AWSLambda.wrapHandler(
+  async (event, context, callback) => {
+    const payload = JSON.parse(event.body);
+    console.log(event);
+    Sentry.setContext("character", {
+      payload: payload,
+      debug: true,
+    });
+
+    const msg = {
+      to: "bzmiller82@gmail.com",
+      from: {
+        email: SENDGRID_FROM_EMAIL,
+        name: fromNameValue,
+      },
+      subject: "ERP Webhook Event",
+      templateId: "d-c110cc501843478596c4e1c2ed47195d",
+      dynamic_template_data: {
+        payloadString: JSON.stringify(payload),
+        payload: payload,
+      },
+    };
+
+    return {
+      statusCode: 200,
+      body: `Webhook received`,
+    };
   }
-})
+);
