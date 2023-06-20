cf96b07ce093a24eeeac97c5a9b6d43af1525bff
bzmillerboy <bzmiller82@gmail.com>
2023-06-19 21:52:31 -0400

    created new endpoint for finance application notifications

---
+++ functions/finapp-notifications/finapp-notifications.js
@@ -0,0 +1,46 @@
+const {
+  HUBSPOT_PORTAL_ID,
+  HUBSPOT_PRIVATE_APP_TOKEN,
+  SENDGRID_API_KEY,
+  SENDGRID_FROM_EMAIL,
+  SENDGRID_FROM_NAME,
+  PORTAL_URL,
+  SUPABASE_URL,
+} = process.env;
+const { createClient } = require("@supabase/supabase-js");
+const Hubspot = require("hubspot");
+const sgMail = require("@sendgrid/mail");
+const hubspot = new Hubspot({
+  accessToken: HUBSPOT_PRIVATE_APP_TOKEN,
+  checkLimit: false,
+});
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY_SERVICE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+exports.handler = async (event) => {
+  const payload = JSON.parse(event);
+  console.log("finapp-notifications payload:", payload);
+  sgMail.setApiKey(SENDGRID_API_KEY);
+
+  // const { data: applications, error: applicationsError } = await supabase
+  //   .from("applications")
+  //   .select("*")
+  //   .eq("id", payload.record.id)
+  //   .single();
+
+  try {
+    // sgMail.setApiKey(SENDGRID_API_KEY);
+
+    return {
+      statusCode: 200,
+      body: `Finance application sent: ${JSON.stringify(data)}`,
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      statusCode: e.statusCode,
+      body: `${e.message} - ${JSON.stringify(e.response.body.errors)}`,
+    };
+  }
+};
