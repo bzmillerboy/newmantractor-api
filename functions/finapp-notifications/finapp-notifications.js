2b16cd885c7e36f0951abbdd90ae657ab091d2c5
bzmillerboy <bzmiller82@gmail.com>
2023-06-21 14:59:36 -0400

    refactor to email lib

--- functions/finapp-notifications/finapp-notifications.js
+++ functions/finapp-notifications/finapp-notifications.js
@@ -1,35 +1,19 @@
-//TODO: make CTA button dynamic based on PORTAL_URL
-
-const {
-  HUBSPOT_PORTAL_ID,
-  HUBSPOT_PRIVATE_APP_TOKEN,
-  SENDGRID_API_KEY,
-  SENDGRID_FROM_EMAIL,
-  SENDGRID_FINAPP_REPLYTO_EMAIL,
-  SENDGRID_FROM_NAME,
-  PORTAL_URL,
-  SUPABASE_URL,
-} = process.env;
+const {} = process.env;
 const { createClient } = require("@supabase/supabase-js");
-const Hubspot = require("hubspot");
-const sgMail = require("@sendgrid/mail");
-const hubspot = new Hubspot({
-  accessToken: HUBSPOT_PRIVATE_APP_TOKEN,
-  checkLimit: false,
-});
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY_SERVICE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
+const emailLib = require("../lib/email-lib.js");
 
 exports.handler = async (event) => {
+  // reject if apikey is not present or incorrect
+  if (event.queryStringParameters.apiKey !== process.env.NEWMANTRACTOR_APIKEY) {
+    return { statusCode: 401, body: "Unauthorized" };
+  }
   const payload = JSON.parse(event.body);
-  // console.log("finapp-notifications payload:", JSON.stringify(payload));
   const appId = payload.record.application_id;
   const activityName = payload.record.name;
-  console.log("apiKey:", event.queryStringParameters.apiKey);
-  // TODO: check apiky and return 401 if not valid
-
-  sgMail.setApiKey(SENDGRID_API_KEY);
+  const toEmail = payload?.record?.metadata?.to_email || "";
 
   const { data: application, error: applicationsError } = await supabase
     .from("applications")
@@ -38,79 +22,32 @@ exports.handler = async (event) => {
     )
     .eq("id", appId)
     .single();
+  if (applicationsError) {
+    console.log("applicationsError:", applicationsError);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Error retrieving application data" }),
+    };
+  }
 
   console.log("application:", JSON.stringify(application));
 
-  const { contact, type, application_id } = application;
-
-  const { first_name, last_name, email } = contact;
-  const { id: typeId } = type;
-
-  const bccEmail = () => {
-    let bccEmail = "";
-    const shouldBCC = payload.record.name === "application submitted";
-    if (typeId === 1 && shouldBCC) {
-      bccEmail = "finance@newmantractor.com";
-    } else if (typeId === 2 && shouldBCC) {
-      bccEmail = "credit@newmantractor.com";
-    }
-
-    return bccEmail, "bzmiller82@gmail.com";
-  };
-  const subject = `Financing Application Submitted | Ref #${application.application_id} | Newman Tractor`; //TODO: make this more dynamic based on application_activity.name
-  const templateId = "d-8e9c9cf1077b4278a413f33c68a7bdca"; //TODO: make this more dynamic based on application_activity.name
-
-  //TODO: improe these to have a fallback for emails that come from notifications@newmantractor.com
-  const fromFirstName = typeId === 1 ? "Matt" : "Caroll";
-  const fromLastName = typeId === 1 ? "Salyers" : "Smith";
-  const fromEmail =
-    typeId === 1 ? "matt@newmantractor.com" : "caroll@newmantractor.com";
-  const fromPhone = typeId === 1 ? "(859) 393-5405" : "(859) 802-5298";
-  const fromJobTitle = typeId === 1 ? "Finance Manager" : "Credit Manager";
-  const fromImage =
-    typeId === 1
-      ? "https://cdn.sanity.io/images/agnoplrn/production/73629f66aaedcf2e9cd482f077520d6af2fe5bc2-3522x3522.jpg?w=600&h=480&q=75&auto=format&fit=crop"
-      : "https://cdn.sanity.io/images/agnoplrn/production/3d170bc7cf16b0fb8f7d9095fbece08f5bba1266-3310x3310.jpg?w=600&h=480&q=75&auto=format&fit=crop";
-
-  const ctaLink = `${PORTAL_URL}/my-applications`;
-  const msg = {
-    to: email,
-    from: {
-      email: SENDGRID_FROM_EMAIL,
-      name: SENDGRID_FROM_NAME,
-    },
-    replyTo: SENDGRID_FINAPP_REPLYTO_EMAIL,
-    bcc: [bccEmail()],
-    subject: subject,
-    templateId: templateId,
-    dynamic_template_data: {
-      email: email,
-      firstName: first_name,
-      lastName: last_name,
-      applicationId: application.application_id,
-      fromImage: fromImage,
-      fromPhone: fromPhone,
-      fromFirstName: fromFirstName,
-      fromLastName: fromLastName,
-      fromPhone: fromPhone,
-      fromEmail: fromEmail,
-      fromJobTitle: fromJobTitle,
-      typeId: typeId,
-      applicationId: application_id,
-      ctaLink: ctaLink,
-    },
-  };
-
-  console.log("msg:", JSON.stringify(msg));
+  // ToEmail
+  // activityName
+  // Should send multiple based on acitivity type
 
   try {
-    await sgMail.send(msg);
+    await emailLib.sendFinanceApplicationEmail(
+      activityName,
+      application,
+      toEmail
+    );
     return { statusCode: 200 };
   } catch (e) {
     console.error(e);
     return {
       statusCode: e.statusCode,
-      body: `${e.message} - ${JSON.stringify(e.response.body.errors)}`,
+      body: `${e.message} - ${JSON.stringify(e?.response)}`,
     };
   }
 };
---
+++ functions/lib/email-lib.js
@@ -0,0 +1,132 @@
+const {
+  HUBSPOT_PORTAL_ID,
+  HUBSPOT_PRIVATE_APP_TOKEN,
+  SENDGRID_API_KEY,
+  SENDGRID_FROM_EMAIL,
+  SENDGRID_FINAPP_REPLYTO_EMAIL,
+  SENDGRID_FROM_NAME,
+  PORTAL_URL,
+  SUPABASE_URL,
+} = process.env;
+
+const sgMail = require("@sendgrid/mail");
+
+const sendFinanceApplicationEmail = async (
+  activityName,
+  application,
+  toEmailValue
+) => {
+  sgMail.setApiKey(SENDGRID_API_KEY);
+
+  // Extract data from application fetch
+  const { contact, type, application_id } = application;
+  const { first_name, last_name, email: contactEmail } = contact;
+  const { id: typeId } = type;
+
+  // Set variables based on application type and activity name
+  let templateId = "";
+  let toEmail = "";
+  let subject = "";
+  let fromEmail = "";
+  let fromName = "";
+  let replyToEmail = "";
+  let bccEmail = "";
+  let fromFirstName = "";
+  let fromLastName = "";
+  let fromPhone = "";
+  let ctaButtonText = "";
+  let ctaButtonLink = "";
+  let fromImage = "";
+  let fromJobTitle = "";
+  const setVariables = async () => {
+    if (activityName === "application submitted" && typeId === 1) {
+      templateId = "d-8e9c9cf1077b4278a413f33c68a7bdca";
+      toEmail = contactEmail;
+      subject = `Financing Application Submitted | Ref #${application.application_id} | Newman Tractor`;
+      fromEmail = "bzmiller82+matt@gmail.com";
+      fromName = "Matt Salyers";
+      replyToEmail = "bzmiller82+matt@gmail.com";
+      bccEmail = "finance@newmantractor.com";
+      fromFirstName = "Matt";
+      fromLastName = "Salyers";
+      fromPhone = "(859) 393-5405";
+      fromJobTitle = "Finance Manager";
+      fromImage =
+        "https://cdn.sanity.io/images/agnoplrn/production/73629f66aaedcf2e9cd482f077520d6af2fe5bc2-3522x3522.jpg?w=600&h=480&q=75&auto=format&fit=crop";
+      ctaButtonLink = `${PORTAL_URL}/my-applications`;
+      ctaButtonText = "VIEW MY APPLICATION";
+    } else if (activityName === "application submitted" && typeId === 2) {
+      templateId = "d-8e9c9cf1077b4278a413f33c68a7bdca";
+      toEmail = contactEmail;
+      subject = `Financing Application Submitted | Ref #${application.application_id} | Newman Tractor`;
+      fromEmail = "bzmiller82+caroll@gmail.com";
+      fromName = "Caroll Smith";
+      replyToEmail = "bzmiller82+caroll@gmail.com";
+      bccEmail = "credit@newmantractor.com";
+      fromFirstName = "Caroll";
+      fromLastName = "Smith";
+      fromPhone = "(859) 802-5298";
+      fromJobTitle = "Credit Manager";
+      fromImage =
+        "https://cdn.sanity.io/images/agnoplrn/production/3d170bc7cf16b0fb8f7d9095fbece08f5bba1266-3310x3310.jpg?w=600&h=480&q=75&auto=format&fit=crop";
+      ctaButtonLink = `${PORTAL_URL}/my-applications`;
+      ctaButtonText = "VIEW MY APPLICATION";
+    } else if (activityName === "sent to lender") {
+      //TODO: include notes in email
+      templateId = "d-8f19bf394e2c4c518636551836b346d9";
+      toEmail = contactEmail;
+      subject = `Financing Application Under Lender Review | Ref #${application.application_id} | Newman Tractor`;
+      fromEmail = "bzmiller82+matt@gmail.com";
+      fromName = "Matt Salyers";
+      replyToEmail = "bzmiller82+matt@gmail.com";
+      bccEmail = "finance@newmantractor.com";
+      fromFirstName = "Matt";
+      fromLastName = "Salyers";
+      fromPhone = "(859) 393-5405";
+      fromJobTitle = "Finance Manager";
+      fromImage =
+        "https://cdn.sanity.io/images/agnoplrn/production/73629f66aaedcf2e9cd482f077520d6af2fe5bc2-3522x3522.jpg?w=600&h=480&q=75&auto=format&fit=crop";
+      ctaButtonLink = `${PORTAL_URL}/my-applications`;
+      ctaButtonText = "VIEW MY APPLICATION";
+    }
+  };
+
+  await setVariables();
+
+  const msg = {
+    to: toEmail,
+    from: {
+      email: fromEmail || SENDGRID_FROM_EMAIL,
+      name: fromName || SENDGRID_FROM_NAME,
+    },
+    replyTo: replyToEmail || SENDGRID_FROM_EMAIL,
+    bcc: [bccEmail],
+    subject: subject,
+    templateId: templateId,
+    dynamic_template_data: {
+      email: toEmail,
+      firstName: first_name,
+      lastName: last_name,
+      applicationId: application.application_id,
+      fromImage: fromImage,
+      fromPhone: fromPhone,
+      fromFirstName: fromFirstName,
+      fromLastName: fromLastName,
+      fromPhone: fromPhone,
+      fromEmail: fromEmail,
+      fromJobTitle: fromJobTitle,
+      typeId: typeId,
+      applicationId: application_id,
+      ctaButtonText: ctaButtonText,
+      ctaButtonLink: ctaButtonLink,
+    },
+  };
+
+  console.log("msg:", JSON.stringify(msg));
+
+  await sgMail.send(msg);
+};
+
+module.exports = {
+  sendFinanceApplicationEmail,
+};
