d861b378af31ee4ea36f90acfc25e4c5f0c16848
bzmillerboy <bzmiller82@gmail.com>
2023-06-23 15:16:39 -0400

    test parse

--- functions/finapp-notifications/finapp-notifications.js
+++ functions/finapp-notifications/finapp-notifications.js
@@ -8,8 +8,9 @@ const emailLib = require("../lib/email-lib.js");
 exports.handler = async (event) => {
   console.log("event:", event);
   // reject if apikey is not present or incorrect
-  console.log("apikey:", event.queryStringParameters.apiKey);
-  console.log("apikey env var:", event.queryStringParameters.apiKey);
+  console.log("queryStringParameters:", event.queryStringParameters);
+  const apikeyParsed = JSON.parse(event.queryStringParameters.apiKey);
+  console.log("apikeyParsed:", apikeyParsed);
   if (event.queryStringParameters.apiKey !== process.env.NEWMANTRACTOR_APIKEY) {
     console.log("Not Authorized - Invalid API Key");
     return { statusCode: 401, body: "Unauthorized" };
